de1002a829af8dea127d1188bc8382dc1517ef01
3dodam <129016953+3dodam@users.noreply.github.com>
2023-07-12 01:24:55 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
@@ -1,13 +1,3 @@
-// function solution(num1, num2) {
-//     let answer = Math.floor(num1/num2)
-//     //Math.floor() 소수점 버림
-//     return answer;
-// }
-
-// let solution = (num1,num2) => {
-//     return Math.floor(num1/num2);
-// };
-
 function solution (num1,num2) {
     return Math.floor(num1/num2);
 };
